306caad8b0412a8af414100ac35ae9d1553c0f29
aqb47 <aquibazmal@gmail.com>
2026-05-27 22:04:39 +0600

    Implemented linked list functions

--- linkedlist.c
+++ linkedlist.c
@@ -1,65 +1,311 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "linkedlist.h"
 
+// Internal helpers
+Node* node_at(LinkedList* linkedlist, int index);
+
 LinkedList ll_create(void) {
-    // TODO
+    LinkedList linkedlist;
+
+    linkedlist.size = 0;
+    linkedlist.head = NULL;
+    linkedlist.tail = NULL;
+
+    return linkedlist;
 }
 
 void ll_free(LinkedList* linkedlist) {
-    // TODO
+    Node* current_node = linkedlist->head;
+
+    // Traverse through linked list starting from head
+    while(current_node != NULL) {
+        // Keep track of current node
+        Node* temp_node = current_node;
+
+        // Move to next node
+        current_node = current_node->next;
+
+        // Free temporary node
+        free(temp_node);
+    }
+
+    // Reset values
+    linkedlist->size = 0;
+    linkedlist->head = NULL;
+    linkedlist->tail = NULL;
 }
 
 int ll_push_back(LinkedList* linkedlist, void* item) {
-    // TODO
+    // Get tail
+    Node* prev_tail = linkedlist->tail;
+    
+    // Make new node and initialize data
+    Node* new_node = (Node*) malloc(sizeof(Node));
+    
+    // If something goes wrong
+    if (new_node == NULL) {
+        return 1;
+    }
+
+    new_node->data = item;
+    new_node->prev = prev_tail;
+    new_node->next = NULL;
+
+    // Append to tail
+    linkedlist->tail = new_node;
+
+    // In case of an empty linked list
+    if (prev_tail == NULL) {
+        linkedlist->head = new_node;
+    }
+
+    // Increment size
+    linkedlist->size += 1;
+
+    return 0;
 }
 
 int ll_push_front(LinkedList* linkedlist, void* item) {
-    // TODO
+    // Get head
+    Node* prev_head = linkedlist->head;
+
+    // Make new node and initialize data
+    Node* new_node = (Node*) malloc(sizeof(Node));
+
+    // If something goes weong
+    if (new_node == NULL) {
+        return 1;
+    }
+
+    new_node->data = item;
+    new_node->prev = NULL;
+    new_node->next = prev_head;
+
+    // Prepend to head
+    linkedlist->head = new_node;
+
+    // In case of an empty linked list
+    if (prev_head == NULL) {
+        linkedlist->tail = new_node;
+    }
+
+    // Increment size
+    linkedlist->size += 1;
+
+    return 0;
 }
 
 void* ll_pop_back(LinkedList* linkedlist) {
-    // TODO
+    // Nothing to pop if linked list was empty
+    if (linkedlist->size == 0) {
+        return NULL;
+    }
+
+    Node* prev_tail = linkedlist->tail;
+    void* prev_tail_item = prev_tail->data;
+
+    // Set current tail of linked list as previous node to previous tail
+    if (linkedlist->size == 1) {
+        linkedlist->head = NULL;
+        linkedlist->tail = NULL;
+    }
+    else {
+        linkedlist->tail = prev_tail->prev;
+        linkedlist->tail->next = NULL;
+    }
+
+    // Free previous tail since it isn't part of the linked list anymore
+    free(prev_tail);
+
+    // Decrement size of linked list
+    linkedlist->size -= 1;
+
+    return prev_tail_item;
 }
 
 void* ll_pop_front(LinkedList* linkedlist) {
-    // TODO
+    // Nothing to pop if linked list is empty
+    if (linkedlist->size == 0) {
+        return NULL;
+    }
+
+    Node* prev_head = linkedlist->head;
+    void* prev_head_item = prev_head->data;
+
+    // Set current head of linked list as next node of previous head
+    if (linkedlist->size == 1) {
+        linkedlist->head = NULL;
+        linkedlist->tail = NULL;
+    }
+    else {
+        linkedlist->head = prev_head->next;
+        linkedlist->head->prev = NULL;
+    }
+
+    // Free previous head since it isn't part of the linked list anymore
+    free(prev_head);
+
+    // Decrement size of linked list
+    linkedlist->size -= 1;
+
+    return prev_head_item;
 }
 
 void* ll_get(LinkedList* linkedlist, int index) {
-    // TODO
+    // Check for valid usage
+    if (index >= linkedlist->size || index < 0) {
+        return NULL;
+    }
+
+    // Get node at that index
+    Node* node_at_index = node_at(linkedlist, index);
+
+    // Return data of node
+    return node_at_index->data;
 }
 
-int ll_insert(LinkedList* linkedlist, int index, void* data) {
-    // TODO
+int ll_insert(LinkedList* linkedlist, int index, void* item) {
+    // Check for valid usage (> instead of >= as item can be inserted at end of list)
+    if (index > linkedlist->size || index < 0) {
+        return 1;
+    }
+
+    // Initialize new node
+    Node* new_node = (Node*) malloc(sizeof(Node));
+    
+    // In case something goes wrong
+    if (new_node == NULL) {
+        return 1;
+    }
+
+    new_node->data = item;
+
+    // Get node at index to be inserted
+    Node* node_at_insert = node_at(linkedlist, index);
+
+    // For an empty list and insertion into first index
+    if (index == 0 && linkedlist->size == 0) {
+        new_node->prev = NULL;
+        new_node->next = NULL;
+
+        linkedlist->head = new_node;
+        linkedlist->tail = new_node;
+    }
+    // In case of insertion at end of non-empty list
+    else if (index == linkedlist->size ) {
+        new_node->prev = linkedlist->tail;
+        new_node->next = NULL;
+
+        linkedlist->tail->next = new_node;
+        linkedlist->tail = new_node;
+    }
+    // In case of insertion at beginning of non-empty list
+    else if (index == 0) {
+        new_node->prev = NULL;
+        new_node->next = linkedlist->head;
+
+        linkedlist->head->prev = new_node;
+        linkedlist->head = new_node;
+    }
+    // Normal case
+    else {
+        new_node->prev = node_at_insert->prev;
+        new_node->next = node_at_insert;
+        
+        node_at_insert->prev->next = new_node;
+        node_at_insert->prev = new_node;
+    }
+
+    // Increment size
+    linkedlist->size += 1;
+
+    return 0;
 }
 
 int ll_delete(LinkedList* linkedlist, int index) {
-    // TODO
+    // Check for valid usage
+    if (index >= linkedlist->size || index < 0) {
+        return 1;
+    }
+
+    // Get node at index to be deleted
+    Node* node_to_delete = node_at(linkedlist, index);
+
+    // Change previous and next pointers of next and previous nodes of node to be deleted
+    if (node_to_delete->prev != NULL) {
+        node_to_delete->prev->next = node_to_delete->next;
+    }
+
+    if (node_to_delete->next != NULL) {
+        node_to_delete->next->prev = node_to_delete->prev;
+    }
+
+    // In case index is at beginning of list or end of list then head or tail of list has to be changed
+    if (index == 0) {
+        linkedlist->head = node_to_delete->next; 
+    }
+    
+    if (index == linkedlist->size - 1) {
+        linkedlist->tail = node_to_delete->prev;
+    }
+
+    // Free deleted node since it won't be used
+    free(node_to_delete);
+
+    // Decrement size
+    linkedlist->size -= 1;
+
+    return 0;
 }
 
 int ll_size(LinkedList* linkedlist) {
-    // TODO
+    return linkedlist->size;
 }
 
 void ll_print(LinkedList* linkedlist, void (*print_fn) (void*)) {
-    // TODO
+    Node* ptr = linkedlist->head;
+
+    while(ptr != NULL) {
+        print_fn(ptr->data);
+
+        printf(" -> ");
+
+        ptr = ptr->next;
+    }
+    printf("NULL\n");
 }
 
 void print_int(void* data) {
-    // TODO
+    printf("%i ", *(int*)data);
 }
 
 void print_double(void* data) {
-    // TODO
+    printf("%lf ", *(double*)data);
 }
 
 void print_char(void* data) {
-    // TODO
+    printf("%c ", *(char *)data);
 }
 
 void print_string(void* data) {
-    // TODO
+    printf("%s ", (char *)data);
 }
 
 void ll_reverse(LinkedList* linkedlist) {
     // TODO
+}
+
+Node* node_at(LinkedList* linkedlist, int index) {
+    Node* current_node = linkedlist->head;
+
+    for (int i = 0; i < index; i++) {
+        current_node = current_node->next;
+
+        if (current_node == NULL) {
+            break;
+        }
+    }
+
+    return current_node;
 }
--- linkedlist.h
+++ linkedlist.h
@@ -1,43 +1,42 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-typedef struct {
-    Node* next;
-    Node* prev;
-    
+typedef struct Node {
+    struct Node* next;
+    struct Node* prev;
     void* data;
 } Node;
 
 typedef struct {
     Node* head;
     Node* tail;
-
     int size;
 } LinkedList;
 
 LinkedList ll_create(void);
-void ll_free(LinkedList* linkedlist);
+void ll_free(LinkedList* linkedlist); // Free all nodes
 
-int ll_push_back(LinkedList* linkedlist, void* item);
-int ll_push_front(LinkedList* linkedlist, void* item);
+int ll_push_back(LinkedList* linkedlist, void* item); // Append to tail
+int ll_push_front(LinkedList* linkedlist, void* item); // Prepend to head
 
-void* ll_pop_back(LinkedList* linkedlist);
-void* ll_pop_front(LinkedList* linkedlist);
+void* ll_pop_back(LinkedList* linkedlist); // Remove tail
+void* ll_pop_front(LinkedList* linkedlist); // Remove head
 
-void* ll_get(LinkedList* linkedlist, int index);
+void* ll_get(LinkedList* linkedlist, int index); // Return node at 0-based index
 
-int ll_insert(LinkedList* linkedlist, int index, void* data);
-int ll_delete(LinkedList* linkedlist, int index);
+int ll_insert(LinkedList* linkedlist, int index, void* item); // Insert node at index 
+int ll_delete(LinkedList* linkedlist, int index); // Remove node at index
 
-int ll_size(LinkedList* linkedlist);
+int ll_size(LinkedList* linkedlist); // Return size of linked list
 
-void ll_print(LinkedList* linkedlist, void (*print_fn) (void*));
+void ll_print(LinkedList* linkedlist, void (*print_fn) (void*)); // Print entire linked list
 
+// Helpers for printing
 void print_int(void* data);
 void print_double(void* data);
 void print_char(void* data);
 void print_string(void* data);
 
-void ll_reverse(LinkedList* linkedlist);
+void ll_reverse(LinkedList* linkedlist); // Reverse linked list
 
 #endif
